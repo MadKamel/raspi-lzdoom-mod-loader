18c767e8d02a3c91b96c38dcfea7126fe257bd10
Eric Rustrum <rustrumek@gmail.com>
2020-08-17 09:58:49 -0700

    Create moddata.py

---
+++ moddata.py
@@ -0,0 +1,9 @@
+//Add your mods here, format is-- 
+// names: Display name of mod
+// desc:  Display description of mod
+// args:  Command-Line LZDOOM argument, -file or otherwise
+
+
+names = []
+desc = []
+args = []
